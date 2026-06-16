cade606fc92af7039ecfb3bca6f317c8ff5c7c04
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2026-06-16 10:45:49 +0530

    first commit

--- Day1/training/app.js
+++ Day1/training/app.js
@@ -1,1 +1,22 @@
-console.log("helloWorld");
+let a = 5;
+let b = 10;
+
+function sum(a, b) {
+  return a + b;
+}
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const square = (a) => a * a;
+
+const numbers = [1, 2, 3, 4, 5];
+const mixed = [1, "adi", true, null, { name: "adi" }];
+// console.log(numbers);
+// console.log(mixed);
+//Array declaration using array constructor
+const emptyArray = new Array(5);
+const arr = new Array(1, 2, 3, 4, 5);
+// console.log(emptyArray);
+// console.log(arr);
