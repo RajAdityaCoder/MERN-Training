8541596a9b3d34bc7a105e9364506e17ea7c470d
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2026-06-16 12:16:22 +0530

    JS Training

--- Day1/training/app.js
+++ Day1/training/app.js
@@ -1,22 +1,55 @@
-let a = 5;
-let b = 10;
+// let a = 5;
+// let b = 10;
 
-function sum(a, b) {
-  return a + b;
-}
+// function sum(a, b) {
+//   return a + b;
+// }
 
-const add = (a, b) => {
-  return a + b;
-};
+// const add = (a, b) => {
+//   return a + b;
+// };
 
-const square = (a) => a * a;
+// const square = (a) => a * a;
 
-const numbers = [1, 2, 3, 4, 5];
-const mixed = [1, "adi", true, null, { name: "adi" }];
-// console.log(numbers);
-// console.log(mixed);
-//Array declaration using array constructor
-const emptyArray = new Array(5);
-const arr = new Array(1, 2, 3, 4, 5);
-// console.log(emptyArray);
+// const numbers = [1, 2, 3, 4, 5];
+// const mixed = [1, "adi", true, null, { name: "adi" }];
+// // console.log(numbers);
+// // console.log(mixed);
+
+// //Array declaration using array constructor
+// const emptyArray = new Array(5);
+// const arr = new Array(1, 2, 3, 4, 5);
+// // console.log(emptyArray);
+// // console.log(arr);
+
+// const arr = [1, 2, 3, 4, 5];
+//slice method
+// console.log(arr.slice(0, 3));
 // console.log(arr);
+//splice method
+// console.log(arr.splice(0, 3));
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.reduce((acc, cur) => acc + cur, 0));
+// console.log(arr.forEach((item, index) => console.log(item, index)));
+
+// const unsorted = [10, 5, 20, 15, 30];
+// console.log(unsorted);
+// console.log(unsorted.sort((a, b) => a - b));
+
+// const object = {
+//   name: "adi",
+//   age: 25,
+//   city: "delhi",
+// };
+// console.log(object.name);
+// console.log(object["age"]);
+// const key = "city";
+// console.log(object[key]);
+
+// const car = { brand: "audi" };
+// car.model = "a4";
+// car.brand = "bmw";
+// console.log(car);
+// delete car.brand;
+// console.log(car);
